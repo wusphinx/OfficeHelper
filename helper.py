c6937ef54cf89eba09b1aa081685f36453c99779
wu.sphinx <wu.sphinx@gmail.com>
2018-09-08 22:33:42 +0800

    Added xls read

--- helper.py
+++ helper.py
@@ -17,13 +17,13 @@ class XlsHelper(object):
         self.check_file_suffix()
 
     def check_file_suffix(self):
-        if len(self.file_path.rsplit("."))<=1:
+        if len(self.file_path.rsplit(".")) <= 1:
             raise SuffixError("xls")
         if self.file_path.rsplit(".")[-1] not in self.XLS_SUFFIX:
             raise SuffixError("xls")
 
     def write(self, field_name_list, data_list):
-        assert len(field_name_list)>0 and len(data_list) >0 
+        assert len(field_name_list)>0 and len(data_list) > 0 
 
         wbk = xlwt.Workbook()
         step = self.MAX_ROW-1
@@ -39,4 +39,27 @@ class XlsHelper(object):
                 for col_num, item in enumerate(row_data):
                     sheet.write(index+1, col_num, item)
         wbk.save(self.file_path)
+    
+    def read(self, sheet_index, row_start, row_end, col_start, col_end):
+        """
+        start from 0
+        """
+        assert row_start >=0 and row_start <= row_end
+        assert col_start >=0 and col_start <= col_end
 
+        xls = xlrd.open_workbook(self.file_path)
+        ss = xls.sheets()
+        assert len(ss) - 1 >= sheet_index
+        sheet = ss[sheet_index]
+
+        assert col_end <= sheet.ncols - 1 
+        assert row_end <= sheet.nrows - 1
+
+        data = list()
+        for row_num in range(row_start, row_end):
+            row_data = list()
+            for col_num in range(col_start, col_end):
+                row_data.append(sheet.row_values(row_num)[col_num])
+            data.append(row_data)
+        
+        return data
--- helper_test.py
+++ helper_test.py
@@ -9,12 +9,22 @@ class TestXlsHelper(unittest.TestCase):
     def test_check_file_suffix(self):
         with self.assertRaises(SuffixError) as cm:
             XlsHelper("ok").check_file_suffix()
+
     def test_write(self):
         data_list = []
         for i in range(65536*2):
             data_list.append([1,'row%d'%(i+1)])
         field_name_list = ['f1', "人生苦短，我用python"]
         XlsHelper("./test1.xls").write(field_name_list, data_list)
+
+    def test_read(self):
+        with self.assertRaises(AssertionError) as cm:
+            XlsHelper("./test1.xls").read(0, 0, 100, 0, 10)
+
+        with self.assertRaises(AssertionError) as cm:
+            XlsHelper("./test1.xls").read(0, 0, 100, 0, 2)
+        
+        XlsHelper("./test1.xls").read(0, 0, 100, 0, 1)
 
 if __name__ == '__main__':
     unittest.main()
