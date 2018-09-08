506e3a4cc86262a991a38b606b3111d8eee9475e
wu.sphinx <wu.sphinx@gmail.com>
2018-09-08 22:48:10 +0800

    Fixed test case

--- helper_test.py
+++ helper_test.py
@@ -1,10 +1,12 @@
 import unittest
+import os 
 from helper import XlsHelper
 from helper import SuffixError
 
 class TestXlsHelper(unittest.TestCase):
-    def test_init(self):
-        pass
+
+    def setUp(self):
+        self.file_path = os.getcwd() + "/test1.xls"
 
     def test_check_file_suffix(self):
         with self.assertRaises(SuffixError) as cm:
@@ -15,16 +17,16 @@ class TestXlsHelper(unittest.TestCase):
         for i in range(65536*2):
             data_list.append([1,'row%d'%(i+1)])
         field_name_list = ['f1', "人生苦短，我用python"]
-        XlsHelper("./test1.xls").write(field_name_list, data_list)
+        XlsHelper(self.file_path ).write(field_name_list, data_list)
 
     def test_read(self):
         with self.assertRaises(AssertionError) as cm:
-            XlsHelper("./test1.xls").read(0, 0, 100, 0, 10)
+            XlsHelper(self.file_path ).read(0, 0, 100, 0, 10)
 
         with self.assertRaises(AssertionError) as cm:
-            XlsHelper("./test1.xls").read(0, 0, 100, 0, 2)
+            XlsHelper(self.file_path ).read(0, 0, 100, 0, 2)
         
-        XlsHelper("./test1.xls").read(0, 0, 100, 0, 1)
+        XlsHelper(self.file_path ).read(0, 0, 100, 0, 1)
 
 if __name__ == '__main__':
     unittest.main()
